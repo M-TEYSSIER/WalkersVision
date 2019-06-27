1c11eb88ec1108e0bd0b880274b12938af54d660
M-TEYSSIER <maxime.teyssier@laposte.net>
2019-06-27 16:49:11 +0200

    problème avec le resultat

--- test/camera/dev.cpp
+++ test/camera/dev.cpp
@@ -2,12 +2,18 @@
 #include "../opencv/highgui.hpp"
 #include <iostream>
 #include <cmath>
+
 using namespace cv;
 using namespace std;
-int i,j,score=89925,res, R,G,B,R1,G1,B1,R2,G2,B2;
-int Calcul(int R1,int R2,int G1, int G2, int B1, int B2){
-        sqrt(pow(abs(R1-R2),2)+pow(abs(G1-G2),2)+pow(abs(B1-B2),2));
+
+int i,j,score=150, R,G,B,R1,G1,B1,R2,G2,B2; 
+
+int Calcul(uchar R1,uchar R2,uchar G1,uchar G2,uchar B1,uchar B2){
+        int a=sqrt(pow(abs(R1-R2),2)+pow(abs(G1-G2),2)+pow(abs(B1-B2),2));
+        printf("%d      ",a);
+        return a;
 }
+
 int main(int argc, char **argv){
         VideoCapture cap(atoi(argv[1]));
         Mat webcam, reference, resultat(640,480,CV_8UC3);
@@ -30,17 +36,20 @@ int main(int argc, char **argv){
                                 R2 = colorF[2];
                                 G2 = colorF[1];
                                 B2 = colorF[0];
+
                                if(score < Calcul(R1,R2,G1,G2,B1,B2)){
-                                        R=G=B=200; 
-                                        printf("LOOOL");
-                                } else{
+                                        R=G=B=0; 
+                                        printf("Mise à 0\n");
+                                } else if(score > Calcul(R1,R2,G1,G2,B1,B2)){
                                         R=colorW[2]; 
                                         G=colorW[1]; 
                                         B=colorW[0]; 
-                                }
+                                        printf("On laisse les couleurs\n");
+                                                
+                               }
                         }
                 }
-                imshow("Final",resultat);
+                imshow("Final", resultat);
                 if(waitKey(30)>=0){break;}
         }
         cap.release();
