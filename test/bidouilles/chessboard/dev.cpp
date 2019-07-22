047dc8a34c783e36076c39047ffeac95c8e890ce
M-TEYSSIER <maxime.teyssier@laposte.net>
2019-07-22 19:59:36 +0200

    Recherche probleme dans dev.cpp | test de calibration 2 camera stereo_calib.cpp (fork)

--- test/bidouilles/chessboard/dev.cpp
+++ test/bidouilles/chessboard/dev.cpp
@@ -13,21 +13,20 @@ using namespace std;
 int main(int argc, char * argv[]) {
         VideoCapture cap(atoi(argv[1]));        
 	if (!cap.isOpened ()){return -1;}
-
-        Size patternsize1(atoi(argv[2]),atoi(argv[3]));	//number of centers
-	Size patternsize2(atoi(argv[4]),atoi(argv[5]));
+        Size patternsize1(atoi(argv[2]),atoi(argv[3])), patternsize2(atoi(argv[4]),atoi(argv[5]));
 	Mat frame, frame2;
 	vector < Point2f > centers, centers2;
-	
+
 	while (1) {
                 cap>>frame; cap>>frame2;
 
                 bool patternfound = findChessboardCorners(frame, patternsize1, centers);
                 drawChessboardCorners(frame, patternsize1, Mat(centers),patternfound);
 
-		// Probleme avec le cercle en vertical (ex: 8x12, les reperes pop à cotés)
+		// Probleme avec les reperes  (ex: 8x12, les reperes pop en sur une zone fixe 
+		// et pas en suivi sur l'objet)
                 bool found = findCirclesGrid(frame2, patternsize2, centers2);
-                drawChessboardCorners(frame2, patternsize2, Mat(centers2),found);
+		drawChessboardCorners(frame2, patternsize2, Mat(centers2),found);
 
 		imshow("Carres", frame);
 		imshow("Cercles", frame2);
